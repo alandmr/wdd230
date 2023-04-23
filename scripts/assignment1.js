db6cfd9fd122f9ac3554edcbfac5d048224aedb2
alandmr <fenix_3006@hotmail.com>
2023-04-23 05:15:56 -0400

    Assingment 01 finished

---
+++ scripts/assignment1.js
@@ -0,0 +1,3 @@
+let currDate = new Date();
+document.querySelector("#updated").textContent = "Last updated: "+currDate.toLocaleString();
+document.querySelector("#year").textContent = "© "+currDate.getFullYear()+" .:|:. Aland D. Muñoz .:|:. Puerto Ordaz - Venezuela";
