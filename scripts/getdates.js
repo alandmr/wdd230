734c0c1e85a6ef6a495a5409cae8fe73602db222
alandmr <fenix_3006@hotmail.com>
2024-01-21 05:01:48 -0400

    01 Assignment Home Page Completed

---
+++ scripts/getdates.js
@@ -0,0 +1,3 @@
+let currDate = new Date();
+document.querySelector("#updated").textContent = "Last modification: "+currDate.toLocaleString();
+document.querySelector("#year").textContent = "© "+currDate.getFullYear()+" .:|:. Aland D. Muñoz .:|:. Barcelona - Venezuela";
