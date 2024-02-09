af5081bffffa34a16f9e454743501de8c1319df5
alandmr <fenix_3006@hotmail.com>
2024-02-09 15:42:45 -0400

    Home page updated lesson03

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,3 +1,11 @@
 let currDate = new Date();
 document.querySelector("#updated").textContent = "Last modification: "+currDate.toLocaleString();
 document.querySelector("#year").textContent = "© "+currDate.getFullYear()+" .:|:. Aland D. Muñoz .:|:. Barcelona - Venezuela";
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
