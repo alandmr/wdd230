c702dd9905b760e586f8ea8590687420867f2d0a
alandmr <fenix_3006@hotmail.com>
2024-03-17 02:57:29 -0400

    Home page continued finished

--- chamber/scripts/getdates.js
+++ chamber/scripts/getdates.js
@@ -10,3 +10,21 @@ hamButton.addEventListener('click', () => {
 	hamButton.classList.toggle('open');
 });
 
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector("main");
+const body = document.querySelector("body");
+
+modeButton.addEventListener("click", () => {
+	if (modeButton.textContent.includes("🕶️")) {
+		main.style.background = "#000";		
+		body.style.background = "#000";		
+		main.style.color = "#fff";
+		modeButton.textContent = "🔆";
+	} else {
+		body.style.background = "#E7ECEF";
+		main.style.background = "#E7ECEF";
+		main.style.color = "#000";
+		modeButton.textContent = "🕶️";
+	}
+});
+
