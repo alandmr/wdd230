d923f51bd32cf792a4b0bd2469bc1e173066b77e
alandmr <fenix_3006@hotmail.com>
2024-04-01 07:32:24 -0400

    updated links.js

--- scripts/links.js
+++ scripts/links.js
@@ -1,5 +1,5 @@
-const baseURL = "https://github.com/alandmr/wdd230/";
-const linksURL = "https://github.com/alandmr/wdd230/data/links.json";
+const baseURL = "https://alandmr.github.io/wdd230/";
+const linksURL = "https://alandmr.github.io/wdd230/data/links.json";
 
 async function getLinks() {
     const response = await fetch(linksURL);
